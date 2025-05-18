a45275a13e592ea533390d317d3dfe05fde14ba7
Josh Hendershot <hendershotjoshr@gmail.com>
2025-05-17 18:05:44 -0600

    feat: add sqlite dice tracking

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module joshhend.dev/home-server
 
 go 1.23.8
+
+require github.com/mattn/go-sqlite3 v1.14.28
--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -11,4 +13,13 @@ func RenderPage(w http.ResponseWriter, pagePath string, pageData any) error {
 		return err
 	}
 	return tmpl.ExecuteTemplate(w, "base", pageData)
+}
+
+func InternalServerError(w http.ResponseWriter, err error) {
+	log.Printf("[ERROR] %v\n", err)
+	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+}
+
+func BadRequest(w http.ResponseWriter, msg string) {
+	http.Error(w, fmt.Sprintf("Bad Request: %v", msg), http.StatusBadRequest)
 }
