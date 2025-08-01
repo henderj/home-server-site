55f3246bc8580d91869a0a84d3f49ed8fa8585dc
Josh Hendershot <hendershotjoshr@gmail.com>
2025-08-01 07:21:26 -0600

    chore: Add rebuild script

---
+++ rebuild.sh
@@ -0,0 +1,7 @@
+set -e
+
+git pull
+docker build -t website:latest .
+# currently, the nixos config is setup to restart this service when it's stopped.
+# so, to restart the service with the new image, we just stop it and let nixos restart it.
+docker stop website 
