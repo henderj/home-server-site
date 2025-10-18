b2feeabe8eb27a605e238c464190b8731fcbfd3a
Josh Hendershot <hendershotjoshr@gmail.com>
2025-10-18 15:45:33 +0000

    update rebuild script for new configuration

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,5 @@
 set -e
 
-# git pull
+git pull
 docker build -t website:latest .
-# currently, the nixos config is setup to restart this service when it's stopped.
-# so, to restart the service with the new image, we just stop it and let nixos restart it.
 docker compose -f /srv/website/compose.yml up -d
